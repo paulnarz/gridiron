52979aedc98d5768f6dcec493d92aa3a2da79bde
unknown <pbednarz@PC-PBednarz.ESRB.ORG>
2016-11-23 14:54:26 -0500

    adding network graph

--- nnpocvs/nnpocvs/Scripts/nnviz/controllers/PlotController.ts
+++ nnpocvs/nnpocvs/Scripts/nnviz/controllers/PlotController.ts
@@ -4,16 +4,18 @@
     export class PlotController {
         Network: nnpoc.Network;
         Points: { x: number, y: number }[];
-        Data: { x: number, y: number, z: number }[];
-        NData: { x: number, y: number, z: number }[];
-        Graph: any;
-        NGraph: any;
-        PlotOtions: any = {
+
+        Graph3d: any;
+        Graph3dData: { x: number, y: number, z: number }[];
+        Graph3dOptions: any = {
             style: 'surface',
             zMin: 0,
             zMax: 1,
         };
-        NPlotOtions: any = {
+
+        Graph2dNormal: any;
+        Graph2dNormalData: { x: number, y: number, z: number }[];
+        Graph2dNormalOptions: any = {
             style: 'surface',
             zMin: 0,
             zMax: 1,
@@ -21,22 +23,24 @@
             cameraPosition: { horizontal: 0.0, vertical: 3.14 }
         };
 
+        GraphNetwork: any;
+        GraphNetworkData: any;
+        GraphNetworkOptions: any = {
+            layout: {
+                hierarchical: {
+                    direction: "LR",
+                    sortMethod: "directed"
+                }
+            }                                    
+        };
+
         constructor(
         ) {
             this.Network = new nnpoc.Network();
-            this.Network.populate(2, [2], 1);
-
             this.createPoints(-1, 1, Math.pow(2, -3));
-            this.calcData();
+            this.initGraphs();
 
-            this.Graph = new vis.Graph3d(document.getElementById('graph'), this.Data, this.PlotOtions);
-            this.NGraph = new vis.Graph3d(document.getElementById('ngraph'), this.NData, this.NPlotOtions);
-        }
-
-        randomize(): void {
-            this.Network.populate(2, [2], 1);
-            this.calcData();
-            this.redraw();
+            this.randomize();
         }
 
         createPoints(min: number, max: number, step: number): void {
@@ -49,9 +53,18 @@
             }
         }
 
+        randomize(): void {
+            this.Network.populate(2, [2], 1);            
+            this.calcData();
+            this.redrawPlot();
+
+            this.Network.calculate([0, 0]);
+            this.redrawNetwork();            
+        }
+
         calcData(): void {
-            this.Data = [];
-            this.NData = [];
+            this.Graph3dData = [];
+            this.Graph2dNormalData = [];
 
             this.Points.forEach(p => {
                 var result = this.Network.calculate([
@@ -61,23 +74,120 @@
 
                 var z = result[0];
 
-                this.Data.push({
+                this.Graph3dData.push({
                     x: p.x,
                     y: p.y,
                     z: z
                 });
 
-                this.NData.push({
+                this.Graph2dNormalData.push({
                     x: p.x,
                     y: p.y,
                     z: z > 0.5 ? 1 : 0
                 });
-            })
+            });
+
+            this.redrawNetwork();
         }
 
-        redraw(): void {
-            this.Graph.setData(this.Data);
-            this.NGraph.setData(this.NData);
+        initGraphs(): void {
+            this.Graph3d = new vis.Graph3d(
+                document.getElementById('graph3d'),
+                this.Graph3dData,
+                this.Graph3dOptions);
+
+            this.Graph2dNormal = new vis.Graph3d(
+                document.getElementById('graph2dNormal'),
+                this.Graph2dNormalData,
+                this.Graph2dNormalOptions);
+
+            this.GraphNetwork = new vis.Network(
+                document.getElementById('graphNetwork'),
+                this.GraphNetworkData,
+                this.GraphNetworkOptions);
+        }
+
+        redrawPlot(): void {
+            this.Graph3d.setData(this.Graph3dData);
+            this.Graph2dNormal.setData(this.Graph2dNormalData);            
+        }
+
+        redrawNetwork(): void {
+            var nodes = [];
+            var edges = [];            
+            var nextID = 1;
+            var idLookUp = {};
+
+            this.Network.layers.forEach((l, i) => {
+                l.neurons.forEach((n, j) => {
+                    var id = i + "_" + j;
+                    idLookUp[id] = nextID++;
+
+                    nodes.push({
+                        id: idLookUp[id],
+                        label: (n.value || 0).toFixed(2),
+                        color: this.getValueColor(n.value || 0)                        
+                    });
+
+                    if (n.weights) {
+                        n.weights.forEach((w, k) => {
+                            var fromID = i - 1 + "_" + k;
+                            edges.push({
+                                from: idLookUp[fromID],
+                                to: idLookUp[id],                                                          
+                                label: w.toFixed(2),
+                                color: this.getWeightColor(w)
+                            });
+                        })
+                    }
+                })
+            });
+            
+            this.GraphNetworkData = {
+                nodes: nodes,
+                edges: edges
+            };            
+
+            this.GraphNetwork.setData(this.GraphNetworkData);
+        }
+
+        getValueColor(value: number): string {
+            var h = 240 - value * 240;
+            return this.hsvToRgb(h, 1, 1);
+        }
+
+        getWeightColor(value: number): string {            
+            var h = 240 - ((value + 4) / 8) * 240;        
+            return this.hsvToRgb(h, 1, 1);
+        }
+
+        hsvToRgb(h: number, v: number, s: number): string {
+            var c = v * s;
+            var hp = h / 60;
+            var x = c * (1 - Math.abs((hp % 2) - 1));
+            var r1, g1, b1;
+            if (hp < 0) {
+                r1 = 0; g1 = 0; b1 = 0;                
+            } else if (hp < 1) {
+                r1 = c; g1 = x; b1 = 0;                
+            } else if (hp < 2) {
+                r1 = x; g1 = c; b1 = 0;                
+            } else if (hp < 3) {
+                r1 = 0; g1 = c; b1 = x;                
+            } else if (hp < 4) {
+                r1 = 0; g1 = x; b1 = c;                
+            } else if (hp < 5) {
+                r1 = x; g1 = 0; b1 = c;                
+            } else if (hp < 6) {
+                r1 = c; g1 = 0; b1 = x;                
+            }
+
+            var m = v - c;
+            var r = r1 + m;
+            var g = g1 + m;
+            var b = b1 + m;
+
+            return 'rgb(' + Math.floor(r * 255) + ',' + Math.floor(g * 255) + ',' + Math.floor(b * 255) + ')';
         }
     }
 
